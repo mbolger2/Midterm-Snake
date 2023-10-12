1943b5d96c3544188a6f5a603a71f41074bbbc02
mbolger2 <mbolger@ltu.edu>
2023-10-12 15:22:55 -0400

    Creation
    Proj creation, prefabs,

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // The maximum and minimum position of the snake in each axis
+    public Vector2 movementBoundariesMin;
+    public Vector2 movementBoundariesMax;
+
+    // The speed of the player in X and Y
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
