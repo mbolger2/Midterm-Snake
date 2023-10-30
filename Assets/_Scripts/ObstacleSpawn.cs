42ac4f08d0e28f4e112e9a0823b3e832ed663740
mbolger2 <mbolger@ltu.edu>
2023-10-30 19:43:20 -0400

    Obstacle
    Obstacles spawn

---
+++ Assets/_Scripts/ObstacleSpawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour
+{
+    // The prefab with the goal will be instantiated
+    public Transform obstaclePrefab;
+
+    // Defining an array to hold the possible spawn points
+    public GameObject[] spawnPoints = new GameObject[81];
+
+    // A counter that holds the number of goals spawned
+    public int spawnCounter = 0;
+
+    // A global variable to help spawn goals
+    //static int tempSpawnCounter = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the counter has surpassed rate
+        if (spawnCounter < 1)
+        {
+            int randomIndex = Random.Range(0, spawnPoints.Length);
+
+            // Message to know where spawn
+            Debug.Log("Spawning obstacle at " + spawnPoints[randomIndex].name);
+
+            // Spawn the goal prefab at the position of the randomly selected
+            // spawnpoint
+            Instantiate(obstaclePrefab, spawnPoints[randomIndex].transform);
+
+            // Reset spawn counter
+            spawnCounter = 1;
+            // tempSpawnCounter = 1;
+        }
+    }
+}
